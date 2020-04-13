31fad32f55b1c5a67c88c8c046612c68d267b49c
mohammadreza.abp <mohammadreza.abp@gmail.com>
2020-04-13 21:33:57 +0430

    Add project files.

---
+++ TestApp/Data/LeaveAllocation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+       
+        public int NumberOfDays { get; set; }
+        [ForeignKey("EmployeeId")]
+        public Employee Employee { get; set; }
+        public string EmployeeId { get; set; }
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public DateTime DateCreated { get; set; }
+
+    }
+}
